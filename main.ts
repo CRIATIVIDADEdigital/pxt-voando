e260f249b849fac9590a759a8630e2d8cecb3929
Admin CRIATIVIDADE.digital <32722216+criatividade-digital@users.noreply.github.com>
2019-03-07 16:48:32 -0300

    Incluido variável para controle do valor da pausa

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let valorDaPausa = 0
 input.onButtonPressed(Button.A, function () {
     Jogo.PassarinhoSubir()
 })
@@ -6,10 +7,11 @@ input.onButtonPressed(Button.B, function () {
 })
 Jogo.CriarPassarinho()
 Jogo.GerarObstaculos()
+valorDaPausa = 1000
 basic.forever(function () {
     Jogo.MoverObstaculos()
     if (Jogo.PassarinhoBateu()) {
         game.gameOver()
     }
-    basic.pause(1000)
+    basic.pause(valorDaPausa)
 })
